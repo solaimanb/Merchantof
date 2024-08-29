7ac50382d2246cdb663a5715e18726dc4d41b890
solaimanb <solaimanb.dev@gmail.com>
2024-08-29 23:16:49 +0600

    prisma schema udpate

---
+++ app/api/products.ts
@@ -0,0 +1,9 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const products = await prisma.product.findMany();
+  res.status(200).json(products);
+}
